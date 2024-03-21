9f09fd0be6d6192c46ee3edc9c6f2edbe1b3141d
Dave Schulze <dave.schulze@clockshark.com>
2024-03-21 16:13:03 -0700

    added preact and jsx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dave-schulze.netlify.app/"
+  site: "https://dave-schulze.netlify.app/",
+  integrations: [preact()]
 });
